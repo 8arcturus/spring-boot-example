0da85e0bca1137473908d09a78806021fc4bbcf4
Sakthi Sivram Palanisamy <Sakthi.Palanisamy@zaplabs.com>
2019-09-19 21:57:18 -0700

    Modify readme

--- src/main/java/com/siv/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/siv/springbootexample/SpringBootExampleApplication.java
@@ -3,6 +3,8 @@ package com.siv.springbootexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
